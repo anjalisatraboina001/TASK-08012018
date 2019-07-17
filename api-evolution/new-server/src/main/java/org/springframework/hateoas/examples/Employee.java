406fe141bf2f3273abd5ba53f6a8e54f744469ad
anjalisatraboina001 <26076934+anjalisatraboina001@users.noreply.github.com>
2019-07-16 20:07:18 -0600

    Update Employee.java

--- api-evolution/new-server/src/main/java/org/springframework/hateoas/examples/Employee.java
+++ api-evolution/new-server/src/main/java/org/springframework/hateoas/examples/Employee.java
@@ -1,18 +1,4 @@
-/*
- * Copyright 2017 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+
 package org.springframework.hateoas.examples;
 
 import lombok.Data;
@@ -27,12 +13,7 @@ import javax.persistence.Id;
 
 import org.springframework.util.StringUtils;
 
-/**
- * An updated domain object where {@literal name} has been replaced by {@literal firstName} and {@literal} lastName. To
- * easy migration, we need to support the old {@literal name} field with a getter and a setter.
- * 
- * @author Greg Turnquist
- */
+
 @Data
 @NoArgsConstructor
 @Entity
@@ -54,20 +35,12 @@ class Employee {
 		return Optional.ofNullable(this.id);
 	}
 
-	/**
-	 * Just merge {@literal firstName} and {@literal lastName} together.
-	 *
-	 * @return
-	 */
+	
 	public String getName() {
 		return this.firstName + " " + this.lastName;
 	}
 
-	/**
-	 * Split things up, and assign the first token to {@literal firstName} with everything else to {@literal lastName}.
-	 *
-	 * @param wholeName
-	 */
+	
 	public void setName(String wholeName) {
 
 		String[] parts = wholeName.split(" ");
